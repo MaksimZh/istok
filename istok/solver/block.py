9161d978563acc08b5dfea951dc1f951279f5718
Maksim Zholudev <maximzms@gmail.com>
2023-02-22 16:56:54 +0300

    Add basic block specs

--- istok/solver/block.py
+++ istok/solver/block.py
@@ -1,11 +1,8 @@
-from typing import Type
-
 from istok.tools import status
 from istok.solver.base import Solver, SolverFactory, is_subtype
 
 
-SolverNodeDescription = \
-    tuple[SolverFactory | Type[Solver], dict[str, str], dict[str, str]]
+SolverNodeDescription = tuple[SolverFactory, dict[str, str], dict[str, str]]
 
 
 class Block(SolverFactory):
@@ -18,8 +15,17 @@ class Block(SolverFactory):
     def __init__(self, description: list[SolverNodeDescription],
             inputs: list[str], outputs: list[str]) -> None:
         super().__init__()
+        self.__input_spec = dict()
+        self.__output_spec = dict()
+        for solver, solver_inputs, solver_outputs in description:
+            solver_input_spec = solver.get_input_spec()
+            solver_output_spec = solver.get_output_spec()
+            for id, link in solver_inputs.items():
+                self.__input_spec[link] = solver_input_spec[id]
+            for id, link in solver_outputs.items():
+                self.__output_spec[link] = solver_output_spec[id]
 
-
+    
     # QUERIES
     
     # Create solver with empty inputs and outputs
@@ -28,8 +34,8 @@ class Block(SolverFactory):
 
     # Get solver input value ids and types
     def get_input_spec(self) -> dict[str, type]:
-        assert False
+        return self.__input_spec
 
     # Get solver output value ids and types
     def get_output_spec(self) -> dict[str, type]:
-        assert False
+        return self.__output_spec
--- tests/test_solver/test_block.py
+++ tests/test_solver/test_block.py
@@ -14,10 +14,10 @@ W = Wrapper(func, ["c", "d"])
 class Test_Single(unittest.TestCase):
     
     def test_spec(self):
-        pass
-        #B = Block([(W, {"a": "aa", "b": "bb"}, {"c": "cc", "d": "dd"})])
-        #self.assertEqual(B.get_input_spec(), {"aa": int, "bb": str})
-        #self.assertEqual(B.get_output_spec(), {"cc": int, "dd": str})
+        B = Block([(W, {"a": "aa", "b": "bb"}, {"c": "cc", "d": "dd"})],
+            inputs=["aa", "bb"], outputs=["cc", "dd"])
+        self.assertEqual(B.get_input_spec(), {"aa": int, "bb": str})
+        self.assertEqual(B.get_output_spec(), {"cc": int, "dd": str})
 
 
 if __name__ == "__main__":
