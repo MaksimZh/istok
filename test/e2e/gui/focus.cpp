188f8dc4e7a597b05cd5e78e90a475dd157c062b
Maksim Zholudev <maximzms@gmail.com>
2025-05-16 23:00:42 +0300

    Add proper focus scenario

--- test/e2e/gui/focus.cpp
+++ test/e2e/gui/focus.cpp
@@ -2,65 +2,77 @@
 #include <catch.hpp>
 /*
     As a user
-    I want to get visual feedback from gui
-    to be sure the application interprets my commands correctly
+    I want all app. windows I can interact look active when the app. is active
+    to see what parts of the app. are ready for my commands
 */
+
+#include <string>
+using namespace std;
 
 
 class Context {
 public:
-    //TODO
-    //WindowManager wm;
-    //Window window;
-
-    void window_created() {
-        //TODO
-        /*
-        window = wm.createWindow("Test", {
-            stackX({
-                spaceX(16),
-                stackY({
-                    spaceY(16),
-                    label("label1"),
-                    spaceY(16),
-                    textField(),
-                    spaceY(16),
-                    button("button1"),
-                    spaceY(16),
-                }),
-                spaceX(16),
-                stackY({
-                    spaceY(16),
-                    label("label2"),
-                    spaceY(16),
-                    textField(),
-                    spaceY(16),
-                    button("button2"),
-                    spaceY(16),
-                }),
-                spaceX(16),
-            })
-        });
-        */
-    }
-
-    void mouse_over_empty_space() {
-        //TODO
-        /*
-        window.mouseMove();
-        */
-    }
-
-    bool all_controls_idle() {
-        //TODO
-        return true;
-    }
+    void app_initialized() {}
+    void app_deactivated() {}
+    void window_created(const string& id) {}
+    void window_activated(const string& id) {}
+    bool window_looks_active(const string& id) {}
 };
 
 
-TEST_CASE("Test Hover", "[gui]") {
+TEST_CASE("Single window activity", "[gui]") {
     Context ctx;
-    ctx.window_created();
-    ctx.mouse_over_empty_space();
-    REQUIRE(ctx.all_controls_idle());
+    // Given
+    ctx.app_initialized();
+    // When
+    ctx.window_created("main");
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == true);
+    // When
+    ctx.app_deactivated();
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == false);
+    // When
+    ctx.window_activated("main");
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == true);
+}
+
+
+TEST_CASE("Two window activity", "[gui]") {
+    Context ctx;
+    // Given
+    ctx.app_initialized();
+    // When
+    ctx.window_created("main");
+    ctx.window_created("tool");
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == true);
+    REQUIRE(ctx.window_looks_active("tool") == true);
+    // When
+    ctx.window_activated("main");
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == true);
+    REQUIRE(ctx.window_looks_active("tool") == true);
+    // When
+    ctx.window_activated("tool");
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == true);
+    REQUIRE(ctx.window_looks_active("tool") == true);
+    // When
+    ctx.app_deactivated();
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == false);
+    REQUIRE(ctx.window_looks_active("tool") == false);
+    // When
+    ctx.window_activated("main");
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == true);
+    REQUIRE(ctx.window_looks_active("tool") == true);
+    // When
+    ctx.app_deactivated();
+    ctx.window_activated("tool");
+    // Then
+    REQUIRE(ctx.window_looks_active("main") == true);
+    REQUIRE(ctx.window_looks_active("tool") == true);
 }
