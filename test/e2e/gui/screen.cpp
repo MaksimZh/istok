2f1194eded0617d78074e8dc07d0b9c8b42b4952
Maksim Zholudev <maximzms@gmail.com>
2025-06-02 23:04:03 +0300

    Add some code to screen

--- test/e2e/gui/screen.cpp
+++ test/e2e/gui/screen.cpp
@@ -8,6 +8,8 @@
 */
 
 #include <vector>
+#include <memory>
+#include <optional>
 
 using namespace std;
 
@@ -21,9 +23,34 @@ struct Rect {
 };
 
 
+class WidgetHandler {};
+
+class WindowHandler : public WidgetHandler {};
+
+template <typename T>
+struct Limits {
+    T lower;
+    optional<T> upper;
+};
+
+template <typename T>
+struct Limits2D {
+    Limits<T> x;
+    Limits<T> y;
+};
+
+struct Widget {
+    Limits2D<float> sizing;
+    Rect<float> location;
+    unique_ptr<WidgetHandler> handler;
+};
+
+
 class Context final {
 public:
     Context(const vector<Rect<int>>& monitorLocations) {}
+
+    void create_screen(vector<Widget> children) {}
 
     bool screen_area_is(Rect<int> area) {
         return true;
@@ -31,26 +58,38 @@ public:
 };
 
 
-TEST_CASE("Init on one monitor", "[e2e][gui]") {
+TEST_CASE("Empty screen on one monitor", "[e2e][gui]") {
     // Given
-    vector<Rect<int>> monitorLocations{
-        Rect<int>{0, 0, 640, 480}
-    };
-    // When empty screen created
-    Context ctx(monitorLocations);
+    Context ctx({Rect<int>{0, 0, 200, 100}});
+    // When
+    ctx.create_screen({});
     // Then
     REQUIRE(ctx.screen_area_is(Rect<int>{0, 0, 640, 480}));
 }
 
 
-TEST_CASE("Init on two monitors", "[e2e][gui]") {
+TEST_CASE("Empty screen on two monitors", "[e2e][gui]") {
     // Given
     vector<Rect<int>> monitorLocations{
-        Rect<int>{0, 0, 640, 480},
-        Rect<int>{-640, 0, 0, 480}
+        Rect<int>{0, 0, 200, 100},
+        Rect<int>{-100, -50, 0, 300}
     };
     // When empty screen created
     Context ctx(monitorLocations);
     // Then
-    REQUIRE(ctx.screen_area_is(Rect<int>{-640, 0, 640, 480}));
+    REQUIRE(ctx.screen_area_is(Rect<int>{-100, -50, 200, 300}));
+}
+
+
+TEST_CASE("One window on one monitor", "[e2e][gui]") {
+    // Given
+    Context ctx({Rect<int>{0, 0, 200, 100}});
+    // When
+    ctx.create_screen({
+        Widget{
+            .sizing = { .x = { .lower = 80 }, .y = { .lower } }
+        }
+    });
+    // Then
+    REQUIRE(ctx.screen_area_is(Rect<int>{0, 0, 640, 480}));
 }
