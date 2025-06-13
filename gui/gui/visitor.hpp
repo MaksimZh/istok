8d077d7760c47aaadb0b4df5889be9117cdb16bc
Maksim Zholudev <maximzms@gmail.com>
2025-06-13 22:49:34 +0300

    Success method pointer handling

--- gui/gui/visitor.hpp
+++ gui/gui/visitor.hpp
@@ -1,4 +1,17 @@
 #pragma once
 
 template <typename T>
-class Visitor {};
+class Visitor {
+public:
+    void visit(T& target) {
+        (this->*method)(target);
+    }
+
+protected:
+    using HandlerPtr = void (Visitor::*)(T&);
+    static HandlerPtr method;
+};
+
+
+template <typename T>
+Visitor<T>::HandlerPtr Visitor<T>::method = nullptr;
--- test/unit/gui/test_visitor.cpp
+++ test/unit/gui/test_visitor.cpp
@@ -4,13 +4,39 @@
 #include <gui\visitor.hpp>
 
 
+#include <string>
+
+
 namespace {
 
-    class A {};
+    class A {
+    public:
+        std::string value;
+    };
 
+    class MockVisitor1: public Visitor<A> {
+    public:
+        MockVisitor1() {
+            if (method != nullptr) {
+                return;
+            }
+            method = reinterpret_cast<HandlerPtr>(&MockVisitor1::visitA);
+        }
+
+        void visitA(A& target) {
+            target.value = "A";
+        }
+    };
 }
 
 
-TEST_CASE("Visitor", "[unit][gui]") {
-    Visitor<A> visitor;
+TEST_CASE("Visitor - single", "[unit][gui]") {
+    MockVisitor1 visitor;
+    A a1, a2;
+    REQUIRE(a1.value == "");
+    REQUIRE(a2.value == "");
+    visitor.visit(a1);
+    visitor.visit(a2);
+    REQUIRE(a1.value == "A");
+    REQUIRE(a2.value == "A");
 }
