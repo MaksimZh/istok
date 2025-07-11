2e50f57c4045ebc92020b589a5786a70a1575382
Maksim Zholudev <maximzms@gmail.com>
2025-07-12 00:35:36 +0300

    Add limited counter

--- ecs/ecs/entity.hpp
+++ ecs/ecs/entity.hpp
@@ -1,2 +1,40 @@
 // Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
 #pragma once
+
+#include <cassert>
+
+
+class LimitedCounter {
+public:
+    LimitedCounter(size_t value, size_t sentinel)
+        : value(value), sentinel(sentinel) {}
+    
+    bool isFull() const {
+        return value == sentinel;
+    }
+
+    void extendBy(size_t shift) {
+        sentinel += shift;
+    }
+
+    operator size_t() const {
+        return value;
+    }
+
+    LimitedCounter& operator++() {
+        assert(!isFull());
+        ++value;
+        return *this;
+    }
+
+    LimitedCounter& operator++(int) {
+        assert(!isFull());
+        auto tmp = *this;
+        ++*this;
+        return tmp;
+    }
+
+private:
+    size_t value;
+    size_t sentinel;
+};
--- test/unit/ecs/test_entity.cpp
+++ test/unit/ecs/test_entity.cpp
@@ -5,5 +5,21 @@
 
 
 TEST_CASE("Entity - limited value", "[unit][ecs]") {
-    REQUIRE(true);
+    LimitedCounter c(0, 2);
+    REQUIRE(c == 0);
+    REQUIRE(!c.isFull());
+    ++c;
+    REQUIRE(c == 1);
+    REQUIRE(!c.isFull());
+    c++;
+    REQUIRE(c == 2);
+    REQUIRE(c.isFull());
+    c.extendBy(3);
+    for (int i = 2; i < 5; i++) {
+        REQUIRE(c == i);
+        REQUIRE(!c.isFull());
+        c++;
+    }
+    REQUIRE(c == 5);
+    REQUIRE(c.isFull());
 }
