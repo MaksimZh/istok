66fce13e2908077999bdee2459e83541738ed035
Maksim Zholudev <maximzms@gmail.com>
2025-07-12 00:19:52 +0300

    Add ECS

---
+++ ecs/ecs/entity.hpp
@@ -0,0 +1,2 @@
+// Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
+#pragma once
---
+++ test/unit/ecs/test_entity.cpp
@@ -0,0 +1,9 @@
+// Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
+#include <catch.hpp>
+
+#include <ecs/entity.hpp>
+
+
+TEST_CASE("Entity - limited value", "[unit][ecs]") {
+    REQUIRE(true);
+}
