65d22787a37271a630c18f40455c62b85f3afefb
Maksim Zholudev <maximzms@gmail.com>
2025-07-12 22:54:22 +0300

    Add component addition and reading

--- ecs/ecs/ecs.hpp
+++ ecs/ecs/ecs.hpp
@@ -3,6 +3,7 @@
 
 #include "entity.hpp"
 
+#include <cassert>
 #include <unordered_map>
 #include <typeindex>
 #include <memory>
@@ -21,6 +22,11 @@ public:
         data[e] = component;
     }
 
+    Component& get(Entity e) {
+        assert(has(e));
+        return data[e];
+    }
+
 private:
     std::unordered_map<Entity, Component, Entity::Hash> data;
 };
@@ -32,27 +38,36 @@ public:
 
     template<typename Component>
     bool has(Entity e) const {
-        std::type_index index(typeid(Component));
+        static std::type_index index(typeid(Component));
         auto it = storages.find(index);
         if (it == storages.end()) {
             return false;
         }
-        auto& storage = *static_cast<ComponentStorageOf<Component>*>((*it).second().get());
+        auto& storage = *static_cast<ComponentStorageOf<Component>*>(it->second.get());
         return storage.has(e);
     }
 
     template<typename Component>
     void add(Entity e, Component&& component) {
-        std::type_index index(typeid(Component));
+        static std::type_index index(typeid(Component));
         auto it = storages.find(index);
         if (it == storages.end()) {
             auto result = storages.emplace(
+                index,
                 std::make_unique<ComponentStorageOf<Component>>());
-            assert(result.second() == true);
-            it = result.first();
+            assert(result.second == true);
+            it = result.first;
         }
-        auto& storage = *static_cast<ComponentStorageOf<Component>*>((*it).second().get());
-        storage.insert(e, component);
+        auto& storage = *static_cast<ComponentStorageOf<Component>*>(it->second.get());
+        storage.insert(e, std::move(component));
+    }
+
+    template<typename Component>
+    Component& get(Entity e) {
+        assert(has<Component>(e));
+        static std::type_index index(typeid(Component));
+        auto& storage = *static_cast<ComponentStorageOf<Component>*>(storages[index].get());
+        return storage.get(e);
     }
 
 private:
--- test/unit/ecs/test_ecs.cpp
+++ test/unit/ecs/test_ecs.cpp
@@ -8,17 +8,31 @@ namespace {
         return Entity(EntityIndex(index), EntityGeneration(0));
     }
 
-    struct A { int value; };
-    struct B { int value; };
-    struct C { int value; };
+    struct A {
+        int value;
+        bool operator==(const A&) const = default;
+    };
+
+    struct B {
+        int value;
+        bool operator==(const B&) const = default;
+    };
+
+    struct C {
+        int value;
+        bool operator==(const C&) const = default;
+    };
+
 }
 
 
 TEST_CASE("ECS - component manager", "[unit][ecs]") {
+    ComponentManager manager;
     Entity e0 = fakeEntity(0);
     Entity e1 = fakeEntity(1);
     Entity e2 = fakeEntity(2);
-    ComponentManager manager;
+    
+    // component presence checks
     REQUIRE(manager.has<A>(e0) == false);
     REQUIRE(manager.has<B>(e0) == false);
     REQUIRE(manager.has<C>(e0) == false);
@@ -26,4 +40,19 @@ TEST_CASE("ECS - component manager", "[unit][ecs]") {
     REQUIRE(manager.has<A>(e0) == true);
     REQUIRE(manager.has<B>(e0) == false);
     REQUIRE(manager.has<C>(e0) == false);
+    manager.add(e0, B{0});
+    manager.add(e1, B{1});
+    manager.add(e1, C{1});
+    REQUIRE(manager.has<A>(e0) == true);
+    REQUIRE(manager.has<B>(e0) == true);
+    REQUIRE(manager.has<C>(e0) == false);
+    REQUIRE(manager.has<A>(e1) == false);
+    REQUIRE(manager.has<B>(e1) == true);
+    REQUIRE(manager.has<C>(e1) == true);
+
+    // component reading
+    REQUIRE(manager.get<A>(e0) == A{0});
+    REQUIRE(manager.get<B>(e0) == B{0});
+    REQUIRE(manager.get<B>(e1) == B{1});
+    REQUIRE(manager.get<C>(e1) == C{1});
 }
