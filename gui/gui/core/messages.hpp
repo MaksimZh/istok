8d8abfe4a3a8f592de82c03cfa87cdcda256594b
Maksim Zholudev <maximzms@gmail.com>
2025-07-30 23:26:02 +0300

    Add new message queue

--- gui/gui/core/messages.hpp
+++ gui/gui/core/messages.hpp
@@ -1,8 +1,193 @@
-// component.hpp
+// messages.hpp
 // Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
 #pragma once
 
+#include <queue>
+#include <mutex>
+
 
 namespace Istok::GUI {
+
+template <typename T>
+class SimpleQueue {
+public:
+    SimpleQueue() = default;
+    SimpleQueue(const SimpleQueue&) = delete;
+    SimpleQueue& operator=(const SimpleQueue&) = delete;
+    SimpleQueue(SimpleQueue&&) = default;
+    SimpleQueue& operator=(SimpleQueue&&) = default;
+
+    bool empty() const {
+        return container.empty();
+    }
+
+    void push(T&& value) {
+        container.push(std::move(value));
+    }
+
+    T take() {
+        assert(!container.empty());
+        T value = std::move(container.front());
+        container.pop();
+        return value;
+    }
+
+private:
+    std::queue<T> container;
+};
+
+
+class QueueMutex {
+public:
+    class check_lock {
+    public:
+        check_lock() = delete;
+        check_lock(const check_lock&) = delete;
+        check_lock& operator=(const check_lock&) = delete;
+        check_lock(check_lock&&) = delete;
+        check_lock& operator=(check_lock&&) = delete;
+
+        check_lock(QueueMutex& qm) : lock(qm.mut) {}
+    
+    private:
+        std::lock_guard<std::mutex> lock;
+    };
+
+    class write_lock {
+    public:
+        write_lock() = delete;
+        write_lock(const write_lock&) = delete;
+        write_lock& operator=(const write_lock&) = delete;
+        write_lock(write_lock&&) = delete;
+        write_lock& operator=(write_lock&&) = delete;
+
+        write_lock(QueueMutex& qm) : lock(qm.mut), cv(qm.cv) {}
+        
+        ~write_lock() {
+            cv.notify_one();
+        }
+    
+    private:
+        std::lock_guard<std::mutex> lock;
+        std::condition_variable& cv;
+    };
+
+    class read_lock {
+    public:
+        read_lock() = delete;
+        read_lock(const read_lock&) = delete;
+        read_lock& operator=(const read_lock&) = delete;
+        read_lock(read_lock&&) = delete;
+        read_lock& operator=(read_lock&&) = delete;
+
+        template <typename Q>
+        read_lock(QueueMutex& qm, Q& container) : lock(qm.mut) {
+            qm.cv.wait(lock, [&container]{ return !container.empty(); });
+        }
+    
+    private:
+        std::unique_lock<std::mutex> lock;
+    };
+
+private:
+    std::mutex mut;
+    std::condition_variable cv;
+};
+
+
+template <typename T>
+class SyncQueue {
+public:
+    using value_type = T;
+    
+    SyncQueue() = default;
+    SyncQueue(const SyncQueue&) = delete;
+    SyncQueue& operator=(const SyncQueue&) = delete;
+    SyncQueue(SyncQueue&&) = default;
+    SyncQueue& operator=(SyncQueue&&) = default;
+
+    bool empty() {
+        QueueMutex::check_lock lock(mut);
+        return container.empty();
+    }
+
+    void push(T&& value) {
+        QueueMutex::write_lock lock(mut);
+        container.push(std::move(value));
+    }
+
+    T take() {
+        QueueMutex::read_lock lock(mut, container);
+        assert(!container.empty());
+        return container.take();
+    }
+
+private:
+    QueueMutex mut;
+    SimpleQueue<T> container;
+};
+
+
+template <typename Q>
+class MessageDest;
+
+template <typename Q>
+class MessageSource;
+
+template <typename Q, typename... Args>
+std::pair<MessageDest<Q>, MessageSource<Q>> makeMessageQueue(Args&&... args) {
+    auto container = std::make_shared<Q>(args...);
+    return std::make_pair(
+        MessageDest<Q>(container),
+        MessageSource<Q>(container));
+}
+
+
+template <typename Q>
+class MessageDest {
+public:
+    MessageDest() = delete;
+    MessageDest(const MessageDest&) = delete;
+    MessageDest& operator=(const MessageDest&) = delete;
+    MessageDest(MessageDest&&) = default;
+    MessageDest& operator=(MessageDest&&) = default;
+    
+    void push(Q::value_type&& value) {
+        container->push(std::move(value));
+    }
+
+private:
+    std::shared_ptr<Q> container;
+
+    MessageDest(std::shared_ptr<Q> container) : container(container) {}
+    
+    template <typename U, typename... Args>
+    friend std::pair<MessageDest<U>, MessageSource<U>>
+        makeMessageQueue(Args&&... args);
+};
+
+
+template <typename Q>
+class MessageSource {
+public:
+    MessageSource() = delete;
+    MessageSource(const MessageSource&) = delete;
+    MessageSource& operator=(const MessageSource&) = delete;
+    MessageSource(MessageSource&&) = default;
+    MessageSource& operator=(MessageSource&&) = default;
+    
+    bool empty() { return container->empty(); }
+    Q::value_type take() { return container->take(); }
+
+private:
+    std::shared_ptr<Q> container;
+
+    MessageSource(std::shared_ptr<Q> container) : container(container) {}
+
+    template <typename U, typename... Args>
+    friend std::pair<MessageDest<U>, MessageSource<U>>
+        makeMessageQueue(Args&&... args);
+};
+
 
 } // namespace Istok::GUI
--- test/unit/gui/test_messages.cpp
+++ test/unit/gui/test_messages.cpp
@@ -1,4 +1,4 @@
-// test_component.cpp
+// test_messages.cpp
 // Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
 #include <catch.hpp>
 
@@ -6,7 +6,121 @@
 
 using namespace Istok::GUI;
 
+#include <memory>
+#include <thread>
+#include <chrono>
 
-TEST_CASE("GUI - messages", "[unit][gui]") {
-    REQUIRE(true);
+using namespace std::chrono_literals;
+
+TEST_CASE("GUI messages - simple queue", "[unit][gui]") {
+    SimpleQueue<std::unique_ptr<int>> queue;
+    REQUIRE(queue.empty() == true);
+
+    SECTION("push and take") {
+        auto a = std::make_unique<int>(42);
+        queue.push(std::move(a));
+        REQUIRE(queue.empty() == false);
+        auto b = queue.take();
+        REQUIRE(b != nullptr);
+        REQUIRE(*b == 42);
+        REQUIRE(queue.empty() == true);
+    }
+
+    SECTION("multiple items") {
+        auto a = std::make_unique<int>(0);
+        auto b = std::make_unique<int>(1);
+        auto c = std::make_unique<int>(2);
+        queue.push(std::move(a));
+        queue.push(std::move(b));
+        queue.push(std::move(c));
+        auto a1 = queue.take();
+        auto b1 = queue.take();
+        auto c1 = queue.take();
+        REQUIRE(queue.empty() == true);
+        REQUIRE(*a1 == 0);
+        REQUIRE(*b1 == 1);
+        REQUIRE(*c1 == 2);
+    }
+
+    SECTION("mixed scenario") {
+        queue.push(std::make_unique<int>(0));
+        queue.push(std::make_unique<int>(1));
+        REQUIRE(*queue.take() == 0);
+        queue.push(std::make_unique<int>(2));
+        queue.push(std::make_unique<int>(3));
+        REQUIRE(*queue.take() == 1);
+        REQUIRE(*queue.take() == 2);
+        queue.push(std::make_unique<int>(4));
+        REQUIRE(*queue.take() == 3);
+        REQUIRE(*queue.take() == 4);
+        REQUIRE(queue.empty() == true);
+    }
 }
+
+
+TEST_CASE("GUI messages - synchronized queue", "[unit][gui]") {
+    SyncQueue<int> queue;
+    REQUIRE(queue.empty() == true);
+
+    SECTION("linear usage") {
+        queue.push(0);
+        queue.push(1);
+        REQUIRE(queue.take() == 0);
+        queue.push(2);
+        queue.push(3);
+        REQUIRE(queue.take() == 1);
+        REQUIRE(queue.take() == 2);
+        queue.push(4);
+        REQUIRE(queue.take() == 3);
+        REQUIRE(queue.take() == 4);
+        REQUIRE(queue.empty() == true);
+    }
+
+    SECTION("muti-thread usage") {
+        std::thread second([&queue]{
+            for (int i = 0; i < 20; ++i) {
+                queue.push(std::move(i));
+                std::this_thread::sleep_for(1ms);
+            }
+        });
+        for (int i = 0; i < 20; ++i) {
+            REQUIRE(queue.take() == i);
+            std::this_thread::sleep_for(1ms);
+        }
+        second.join();
+    }
+}
+
+TEST_CASE("GUI messages - message adapters", "[unit][gui]") {
+    auto [dest, source] = makeMessageQueue<SyncQueue<int>>();
+    REQUIRE(source.empty() == true);
+
+    SECTION("linear usage") {
+        dest.push(0);
+        dest.push(1);
+        REQUIRE(source.take() == 0);
+        dest.push(2);
+        dest.push(3);
+        REQUIRE(source.take() == 1);
+        REQUIRE(source.take() == 2);
+        dest.push(4);
+        REQUIRE(source.take() == 3);
+        REQUIRE(source.take() == 4);
+        REQUIRE(source.empty() == true);
+    }
+
+    SECTION("muti-thread usage") {
+        std::thread second([&dest]{
+            for (int i = 0; i < 20; ++i) {
+                dest.push(std::move(i));
+                std::this_thread::sleep_for(1ms);
+            }
+        });
+        for (int i = 0; i < 20; ++i) {
+            REQUIRE(source.take() == i);
+            std::this_thread::sleep_for(1ms);
+        }
+        second.join();
+    }
+}
+
