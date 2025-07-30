71102489f986896d34852ca992c976996539f113
Maksim Zholudev <maximzms@gmail.com>
2025-07-30 15:54:06 +0300

    Add module for messages

---
+++ gui/gui/core/messages.hpp
@@ -0,0 +1,8 @@
+// component.hpp
+// Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
+#pragma once
+
+
+namespace Istok::GUI {
+
+} // namespace Istok::GUI
---
+++ test/unit/gui/test_messages.cpp
@@ -0,0 +1,12 @@
+// test_component.cpp
+// Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
+#include <catch.hpp>
+
+#include <gui/core/messages.hpp>
+
+using namespace Istok::GUI;
+
+
+TEST_CASE("GUI - messages", "[unit][gui]") {
+    REQUIRE(true);
+}
