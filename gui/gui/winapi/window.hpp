6fb11631a4068cad9ec47476ecf3e66b5ddfce3c
Maksim Zholudev <maximzms@gmail.com>
2025-08-10 19:24:39 +0300

    Move low-level stuff to basic.hpp

--- gui/gui/winapi/window.hpp
+++ gui/gui/winapi/window.hpp
@@ -2,18 +2,17 @@
 // Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
 #pragma once
 
+
+#include <gui/core/tools.hpp>
+#include <gui/core/messages.hpp>
+#include <gui/winapi/basic.hpp>
+
 #include <cassert>
-#include <windows.h>
 #include <windowsx.h>
 #include <dwmapi.h>
 #include <stdexcept>
 #include <iostream>
 #include <optional>
-
-#include <gui/core/tools.hpp>
-#include <gui/core/messages.hpp>
-
-#include "gl.hpp"
 
 constexpr UINT WM_APP_QUEUE = WM_APP + 1;
 
@@ -105,108 +104,6 @@ private:
 };
 
 
-class WndHandle {
-public:
-    WndHandle() = default;
-
-    WndHandle(HWND hWnd) : hWnd(hWnd) {}
-
-    ~WndHandle() {
-        clean();
-    }
-
-    WndHandle(const WndHandle&) = delete;
-    WndHandle& operator=(const WndHandle&) = delete;
-
-    WndHandle(WndHandle&& other) noexcept
-        : hWnd(other.hWnd) {
-        other.drop();
-    }
-
-    WndHandle& operator=(WndHandle&& other) noexcept {
-        if (this != &other) {
-            clean();
-            hWnd = other.hWnd;
-            other.drop();
-        }
-        return *this;
-    }
-
-    operator bool() const {
-        return hWnd != nullptr;
-    }
-
-    HWND get() const {
-        return hWnd;
-    }
-
-private:
-    HWND hWnd = nullptr;
-
-    void clean() {
-        if (hWnd != nullptr) {
-            DestroyWindow(hWnd);
-        }
-        drop();
-    }
-
-    void drop() {
-        hWnd = nullptr;
-    }
-};
-
-
-class DCHandle {
-public:
-    DCHandle() = default;
-    
-    DCHandle(HDC hDC) : hDC(hDC) {}
-
-    ~DCHandle() {
-        clean();
-    }
-
-    DCHandle(const DCHandle&) = delete;
-    DCHandle& operator=(const DCHandle&) = delete;
-
-    DCHandle(DCHandle&& other) noexcept
-        : hDC(other.hDC) {
-        other.drop();
-    }
-
-    DCHandle& operator=(DCHandle&& other) noexcept {
-        if (this != &other) {
-            clean();
-            hDC = other.hDC;
-            other.drop();
-        }
-        return *this;
-    }
-
-    operator bool() const {
-        return hDC != nullptr;
-    }
-
-    HDC get() const {
-        return hDC;
-    }
-
-private:
-    HDC hDC = nullptr;
-
-    void clean() {
-        if (hDC != nullptr) {
-            ReleaseDC(WindowFromDC(hDC), hDC);
-        }
-        drop();
-    }
-    
-    void drop() {
-        hDC = nullptr;
-    }
-};
-
-
 std::wstring toUTF16(const std::string& source) {
     int size = MultiByteToWideChar(CP_UTF8, 0, source.c_str(), -1, nullptr, 0);
     if (size == 0) {
@@ -273,68 +170,6 @@ public:
 struct WindowParams {
     Rect<int> location;
     std::optional<std::string> title;
-};
-
-
-class GLManager {
-public:
-    GLManager(WndHandle& wnd)
-        : dc(makeDC(wnd)),
-        gl(std::make_shared<GLHandle>(createModernGLContext(dc.get()))) {}
-
-    GLManager(WndHandle& wnd, const GLManager& other)
-        : dc(makeDC(wnd)), gl(other.gl) {}
-
-    GLManager(const GLManager&) = delete;
-    GLManager& operator=(const GLManager&) = delete;
-    GLManager(const GLManager&&) = delete;
-    GLManager& operator=(const GLManager&&) = delete;
-
-    void activate() {
-        wglMakeCurrent(dc.get(), gl->get());
-    }
-
-    void swapBuffers() {
-        SwapBuffers(dc.get());
-    }
-
-private:
-    DCHandle dc;
-    std::shared_ptr<GLHandle> gl;
-
-    static DCHandle makeDC(WndHandle& wnd) {
-        DCHandle dc(GetWindowDC(wnd.get()));
-        if (!dc) {
-            throw std::runtime_error("Cannot get window DC");
-        }
-        setPixelFormat(dc);
-        return dc;
-    }
-
-    static void setPixelFormat(DCHandle& dc) {
-        PIXELFORMATDESCRIPTOR pfd = {};
-        pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
-        pfd.nVersion = 1;
-        pfd.dwFlags =
-            PFD_DRAW_TO_WINDOW |
-            PFD_SUPPORT_OPENGL |
-            PFD_DOUBLEBUFFER |
-            PFD_SUPPORT_COMPOSITION;
-        pfd.iPixelType = PFD_TYPE_RGBA;
-        pfd.cColorBits = 32;
-        pfd.cAlphaBits = 8;
-        pfd.cDepthBits = 24;
-        pfd.cStencilBits = 8;
-        pfd.iLayerType = PFD_MAIN_PLANE;
-
-        int pfi = ChoosePixelFormat(dc.get(), &pfd);
-        if (!pfi) {
-            throw std::runtime_error("Failed to choose pixel format");
-        }
-        if (!SetPixelFormat(dc.get(), pfi, &pfd)) {
-            throw std::runtime_error("Failed to set pixel format");
-        }
-    }
 };
 
 
