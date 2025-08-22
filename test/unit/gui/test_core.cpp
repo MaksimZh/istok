ab974d3b7a7619a91e6c03948731128f4572b894
Maksim Zholudev <maximzms@gmail.com>
2025-08-22 14:23:43 +0300

    Use InstanceGetter in GUI

--- test/unit/gui/test_core.cpp
+++ test/unit/gui/test_core.cpp
@@ -2,6 +2,8 @@
 #include <catch.hpp>
 
 #include <gui/common/core.hpp>
+#include <tools/queue.hpp>
+#include <tools/helpers.hpp>
 
 using namespace Istok::Tools;
 using namespace Istok::GUI;
@@ -16,81 +18,29 @@ namespace {
 
 using DebugQueue = SyncWaitingQueue<std::string>;
 
-/**
- * @brief Mock for GUI platform-dependent stuff
- * 
- * Pointer to instance can be obtained even if it is created implicitly.
- * Each instance holds its own debug message queue as a shared pointer.
- * Thus the queue is accessible even after instance destruction.
- * 
- */
 template <typename WindowID_>
 class MockPlatform {
 public:
     using WindowID = WindowID_;
     using InQueue = SyncWaitingQueue<GUIMessage<WindowID>>;
 
-    /**
-     * @brief Debug message queue
-     */
     std::shared_ptr<DebugQueue> debugQueue;
 
-    /**
-     * @brief Construct a new MockPlatform instance
-     * 
-     * The pointer to new instance can be obtained via static `release` method.
-     * If there is already an instance that is not released yet the constructor
-     * will wait.
-     */
     MockPlatform(GUIHandler<WindowID>& handler)
-        : handler(&handler), queue(std::make_shared<InQueue>()),
+        : instanceGetter(this),
+        handler(&handler),
+        queue(std::make_shared<InQueue>()),
         debugQueue(std::make_shared<DebugQueue>())
     {
-        std::unique_lock lock(mut);
         debugQueue->push("create");
-        cv.wait(lock, [] { return instance == nullptr; });
-        instance = this;
     }
 
-    /**
-     * @brief Destroy the MockPlatform instance
-     * 
-     * Releases the instance on destruction so that other instances
-     * can be created.
-     */
     ~MockPlatform() {
-        std::lock_guard lock(mut);
         debugQueue->push("destroy");
-        if (instance == this) {
-            instance = nullptr;
-            cv.notify_all();
-        }
     }
 
-    /**
-     * @brief Get pointer to last created instance
-     * 
-     * This method returns the pointer to the last created instance of
-     * this class. Call of this method unlocks creation of other instances.
-     * 
-     * @return MockPlatform* 
-     * 
-     * @details
-     * When instance is created all further constructor calls wait until
-     * the pointer to this instance is obtained by call of this function
-     * or until the destructor is called.
-     * Creation of instances and obtaining pointers to them is thread-safe.
-     * 
-     * @usage
-     * ... // some code creating instance
-     * MockPlatform<T>* instance = MockPlatform<T>::release();
-     */
     static MockPlatform* release() {
-        std::lock_guard lock(mut);
-        MockPlatform* tmp = instance;
-        instance = nullptr;
-        cv.notify_all();
-        return tmp;
+        return InstanceGetter<MockPlatform>::release();
     }
 
     MockPlatform(const MockPlatform&) = delete;
@@ -143,24 +93,13 @@ public:
     }
 
 private:
-    static MockPlatform* instance;
-    static std::mutex mut;
-    static std::condition_variable cv;
+    InstanceGetter<MockPlatform> instanceGetter;
 
     GUIHandler<WindowID>* handler;
     std::shared_ptr<InQueue> queue;
     bool running = false;
     bool sync = false;
 };
-
-template <typename WindowID>
-MockPlatform<WindowID>* MockPlatform<WindowID>::instance = nullptr;
-
-template <typename WindowID>
-std::mutex MockPlatform<WindowID>::mut;
-
-template <typename WindowID>
-std::condition_variable MockPlatform<WindowID>::cv;
 
 }
 
--- tools/tools/helpers.hpp
+++ tools/tools/helpers.hpp
@@ -20,15 +20,35 @@ template<typename T>
 using hash = typename Hash<T>::type;
 
 
+/**
+ * @brief Auxiliary class for retrieving implicitly created instances
+ * 
+ * @param T Type of the instance to handle
+ */
 template <typename T>
 class InstanceGetter {
 public:
+    /**
+     * @brief Construct new instance
+     * 
+     * If previously created pointer is still not released
+     * the constructor waits until it is.
+     * Note that attempt to construct second instance in the same thread
+     * without releasing the first one will result in deadlock.
+     * 
+     * @param self The pointer to store for later usage
+     */
     InstanceGetter(T* self) : self(self) {
         std::unique_lock lock(mut);
         cv.wait(lock, [] { return instance == nullptr; });
         instance = self;
     }
 
+    /**
+     * @brief Destroy the instance
+     * 
+     * Releases the pointer it is still waiting.
+     */
     ~InstanceGetter() {
         std::lock_guard lock(mut);
         if (instance == self) {
@@ -37,6 +57,11 @@ public:
         }
     }
 
+    /**
+     * @brief Get pointer to the last created instance
+     * 
+     * @return T*
+     */
     static T* release() {
         std::lock_guard lock(mut);
         T* tmp = instance;
