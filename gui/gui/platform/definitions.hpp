2a6e02cfcf704c6010a914e98ca62d896e0591bc
Maksim Zholudev <maximzms@gmail.com>
2025-08-25 21:53:58 +0300

    Add new platform definitions

---
+++ gui/gui/platform/definitions.hpp
@@ -0,0 +1,81 @@
+// Copyright 2025 Maksim Sergeevich Zholudev. All rights reserved
+#pragma once
+
+#include <variant>
+#include <type_traits>
+#include <optional>
+#include <string>
+
+namespace Istok::GUI {
+
+namespace Event {
+    /// @brief Response to a heartbeat request, confirming platform is alive
+    /// @note Sent automatically by the platform thread upon receiving HeartbeatRequest
+    struct PlatformHeartbeatResponse {};
+    
+    /// @brief Heartbeat response was not received within the expected time
+    /// @note Indicates the platform may have crashed or become unresponsive
+    struct PlatformHeartbeatTimeout {};
+
+    /// @brief Exception encountered in the platform
+    struct PlatformException {
+        std::exception_ptr exception; ///< Exception stored for later handling
+    };
+
+    /// @brief Platform has shut down
+    struct PlatformShutdown {};
+    
+    /// @brief Request to close a window
+    /// @tparam WindowID Window identifier type
+    template <typename WindowID>
+    struct WindowClose { WindowID id; };
+}
+
+template <typename WindowID>
+using PlatformEvent = std::variant<
+    Event::PlatformHeartbeatResponse,
+    Event::PlatformHeartbeatTimeout,
+    Event::PlatformException,
+    Event::PlatformShutdown,
+    Event::WindowClose
+>;
+
+
+template <typename T>
+struct Rect {
+    T left;
+    T top;
+    T right;
+    T bottom;
+};
+
+struct WindowParams {
+    Rect<int> location;
+    std::optional<std::string> title;
+};
+
+
+template <typename Platform>
+concept GUIPlatform = requires {
+    typename Platform::WindowID;
+    typename Platform::Scene;
+    typename Platform::ScenePatch;
+    requires std::is_default_constructible_v<Platform>;
+} && requires(
+    Platform platform,
+    typename Platform::WindowID id,
+    WindowParams windowParams,
+    Rect<int> location,
+    typename Platform::Scene scene,
+    typename Platform::ScenePatch scenePatch
+) {
+    {platform.getMessage()} noexcept ->
+        std::same_as<PlatformEvent<typename Platform::WindowID>>;
+    {platform.createWindow(id, windowParams)} -> std::same_as<void>;
+    {platform.destroyWindow(id)} -> std::same_as<void>;
+    {platform.setWindowLocation(id, location)} -> std::same_as<void>;
+    {platform.loadScene(id, scene)} -> std::same_as<void>;
+    {platform.patchScene(id, scenePatch)} -> std::same_as<void>;
+};
+
+} // namespace Istok::GUI
