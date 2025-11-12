99ea96f0957522b38a1c304cac1d0dcf7c0df49e
Maksim Zholudev <maximzms@gmail.com>
2025-11-13 00:22:22 +0300

    Make ECS running even in modal loop

--- playground/ecsbased.cpp
+++ playground/ecsbased.cpp
@@ -8,142 +8,6 @@
 
 using namespace Istok::ECS;
 
-template <typename T>
-struct Rect {
-    T left;
-    T top;
-    T right;
-    T bottom;
-};
-
-HINSTANCE getHInstance() {
-    static HINSTANCE hInstance =
-        reinterpret_cast<HINSTANCE>(GetModuleHandle(NULL));
-    return hInstance;
-}
-
-class WindowClass {
-public:
-    WindowClass() = default;
-
-    WindowClass(WNDPROC lpfnWndProc, LPCWSTR className)
-    : name(className) {
-        WNDCLASSEX wcex{};
-        wcex.cbSize = sizeof(WNDCLASSEX);
-        wcex.style = CS_OWNDC;
-        wcex.lpfnWndProc = lpfnWndProc;
-        wcex.cbClsExtra = 0;
-        wcex.cbWndExtra = 0;
-        wcex.hInstance = getHInstance();
-        wcex.hIcon = nullptr;
-        wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-        wcex.hbrBackground = nullptr;
-        wcex.lpszMenuName = nullptr;
-        wcex.lpszClassName = className;
-        wcex.hIconSm = nullptr;
-        if (!RegisterClassEx(&wcex)) {
-            throw std::runtime_error("Failed to register window class.");
-        }
-    }
-
-    ~WindowClass() noexcept {
-        UnregisterClass(name, getHInstance());
-    }
-
-    WindowClass(const WindowClass&) = delete;
-    WindowClass& operator=(const WindowClass&) = delete;
-    WindowClass(WindowClass&& other) = delete;
-    WindowClass& operator=(WindowClass&& other) = delete;
-
-    LPCWSTR get() const {
-        return name;
-    }
-
-private:
-    LPCWSTR name = nullptr;
-};
-
-struct ScreenLocation {
-    Rect<int> value;
-};
-
-namespace WindowHandler {
-
-struct Close {
-    std::function<void()> func;
-};
-
-} // namespace WindowHandler
-
-
-LRESULT CALLBACK windowProc(
-    HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
-{
-    if (msg == WM_CLOSE) {
-        if (BoundEntity* entity = reinterpret_cast<BoundEntity*>(
-            GetWindowLongPtr(hWnd, GWLP_USERDATA))
-        ) {
-            if (entity->has<WindowHandler::Close>()) {
-                entity->get<WindowHandler::Close>().func();
-                return 0;
-            }
-        }
-    }
-    return DefWindowProc(hWnd, msg, wParam, lParam);
-}
-
-
-class Window {
-public:
-    Window(BoundEntity entity, const Rect<int>& location)
-    : entity(std::make_unique<BoundEntity>(entity)) {
-        HWND hWnd = CreateWindowEx(
-            NULL,
-            getWindowClass(),
-            L"Istok",
-            WS_OVERLAPPEDWINDOW,
-            location.left, location.top,
-            location.right - location.left,
-            location.bottom - location.top,
-            NULL, NULL, getHInstance(), nullptr);
-        if (!hWnd) {
-            throw std::runtime_error("Cannot create window");
-        }
-        SetWindowLongPtr(
-            hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(
-                this->entity.get()));
-        ShowWindow(hWnd, SW_SHOW);
-        std::cout << "Create window " << hWnd << std::endl;
-        this->hWnd = std::make_unique<HWND>(hWnd);
-    }
-    
-    ~Window() {
-        if (hWnd && *hWnd) {
-            std::cout << "Destroy window " << *hWnd << std::endl;
-            DestroyWindow(*hWnd);
-        }
-    }
-
-    Window(const Window&) = delete;
-    Window& operator=(const Window&) = delete;
-    Window(Window&&) = default;
-    Window& operator=(Window&&) = default;
-
-    HWND getHWnd() const {
-        return *hWnd;
-    }
-
-private:
-    std::unique_ptr<BoundEntity> entity;
-    std::unique_ptr<HWND> hWnd;
-
-    static LPCWSTR getWindowClass() {
-        static WindowClass wc(windowProc, L"Istok");
-        return wc.get();
-    }
-};
-
-
 class ECSManager;
 
 using System = std::function<void(ECSManager&)>;
@@ -162,15 +26,20 @@ public:
         systems.push_back(system);
     }
 
+    void iterate() {
+        std::cout << "ECS iteration" << std::endl;
+        for (auto& s : systems) {
+            s(*this);
+        }
+    }
+
     void run() {
         if (running) {
             return;
         }
         running = true;
         while (running) {
-            for (auto& s : systems) {
-                s(*this);
-            }
+            iterate();
         }
     }
 
@@ -255,24 +124,202 @@ private:
 };
 
 
+template <typename T>
+struct Rect {
+    T left;
+    T top;
+    T right;
+    T bottom;
+};
+
+HINSTANCE getHInstance() {
+    static HINSTANCE hInstance =
+        reinterpret_cast<HINSTANCE>(GetModuleHandle(NULL));
+    return hInstance;
+}
+
+class WindowClass {
+public:
+    WindowClass() = default;
+
+    WindowClass(WNDPROC lpfnWndProc, LPCWSTR className)
+    : name(className) {
+        WNDCLASSEX wcex{};
+        wcex.cbSize = sizeof(WNDCLASSEX);
+        wcex.style = CS_OWNDC;
+        wcex.lpfnWndProc = lpfnWndProc;
+        wcex.cbClsExtra = 0;
+        wcex.cbWndExtra = 0;
+        wcex.hInstance = getHInstance();
+        wcex.hIcon = nullptr;
+        wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+        wcex.hbrBackground = nullptr;
+        wcex.lpszMenuName = nullptr;
+        wcex.lpszClassName = className;
+        wcex.hIconSm = nullptr;
+        if (!RegisterClassEx(&wcex)) {
+            throw std::runtime_error("Failed to register window class.");
+        }
+    }
+
+    ~WindowClass() noexcept {
+        UnregisterClass(name, getHInstance());
+    }
+
+    WindowClass(const WindowClass&) = delete;
+    WindowClass& operator=(const WindowClass&) = delete;
+    WindowClass(WindowClass&& other) = delete;
+    WindowClass& operator=(WindowClass&& other) = delete;
+
+    LPCWSTR get() const {
+        return name;
+    }
+
+private:
+    LPCWSTR name = nullptr;
+};
+
+struct ScreenLocation {
+    Rect<int> value;
+};
+
+namespace WindowHandler {
+
+struct Close {
+    std::function<void()> func;
+};
+
+} // namespace WindowHandler
+
+
+struct WindowData {
+    ECSManager* ecs;
+    Entity entity;
+};
+
+
+LRESULT CALLBACK windowProc(
+    HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+
+
+class Window {
+public:
+    Window(WindowData data) : data(std::make_unique<WindowData>(data)) {
+        auto entity = data.ecs->bind(data.entity);
+        auto location = entity.get<ScreenLocation>().value;
+        HWND hWnd = CreateWindowEx(
+            NULL,
+            getWindowClass(),
+            L"Istok",
+            WS_OVERLAPPEDWINDOW,
+            location.left, location.top,
+            location.right - location.left,
+            location.bottom - location.top,
+            NULL, NULL, getHInstance(), nullptr);
+        if (!hWnd) {
+            throw std::runtime_error("Cannot create window");
+        }
+        SetWindowLongPtr(
+            hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(
+                this->data.get()));
+        ShowWindow(hWnd, SW_SHOW);
+        std::cout << "Create window " << hWnd << std::endl;
+        this->hWnd = std::make_unique<HWND>(hWnd);
+    }
+    
+    ~Window() {
+        if (hWnd && *hWnd) {
+            std::cout << "Destroy window " << *hWnd << std::endl;
+            DestroyWindow(*hWnd);
+        }
+    }
+
+    Window(const Window&) = delete;
+    Window& operator=(const Window&) = delete;
+    Window(Window&&) = default;
+    Window& operator=(Window&&) = default;
+
+    HWND getHWnd() const {
+        return *hWnd;
+    }
+
+private:
+    std::unique_ptr<WindowData> data;
+    std::unique_ptr<HWND> hWnd;
+
+    static LPCWSTR getWindowClass() {
+        static WindowClass wc(windowProc, L"Istok");
+        return wc.get();
+    }
+};
+
+enum class WindowState {
+    IDLE,
+    PROCESSING,
+    CHANGED
+};
+
+LRESULT CALLBACK windowProc(
+    HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+    WindowData* data = reinterpret_cast<WindowData*>(
+        GetWindowLongPtr(hWnd, GWLP_USERDATA));
+    if (!data) {
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+    auto stateView = data->ecs->view<WindowState>();
+    if (stateView.begin() == stateView.end()) {
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+    auto global = data->ecs->bind(*stateView.begin());
+    global.set(WindowState::IDLE);
+    if (msg == WM_CLOSE) {
+        global.set(WindowState::CHANGED);
+        auto entity = data->ecs->bind(data->entity);
+        if (entity.has<WindowHandler::Close>()) {
+            entity.get<WindowHandler::Close>().func();
+            return 0;
+        }
+    }
+    if (msg == WM_SIZE) {
+        global.set(WindowState::CHANGED);
+        auto entity = data->ecs->bind(data->entity);
+        if (entity.has<Window>()) {
+            std::cout << "message: WM_SIZE" << std::endl;
+            data->ecs->iterate();
+        }
+        return 0;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+
 void createMissingWindows(ECSManager& ecs) {
     for (auto& w : ecs.view<ScreenLocation>().exclude<Window>()) {
         std::cout << "Creating window for entity " << w.value << std::endl;
-        auto e = ecs.bind(w);
-        e.set(Window(e, e.get<ScreenLocation>().value));
+        ecs.set(w, Window(WindowData{&ecs, w}));
     }
 }
 
 
 void processWindowsMessages(ECSManager& ecs) {
-    MSG msg;
-    GetMessage(&msg, NULL, 0, 0);
-    if (msg.message == WM_QUIT) {
-        ecs.stop();
-        return;
+    auto stateView = ecs.view<WindowState>();
+    auto global = ecs.bind(
+        (stateView.begin() == stateView.end())
+            ? ecs.createEntity(WindowState::IDLE)
+            : *stateView.begin());
+    while (global.get<WindowState>() == WindowState::IDLE) {
+        global.set(WindowState::PROCESSING);
+        MSG msg;
+        GetMessage(&msg, NULL, 0, 0);
+        if (msg.message == WM_QUIT) {
+            ecs.stop();
+            return;
+        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
+    global.set(WindowState::IDLE);
 }
 
 
