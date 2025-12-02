90a3ea162fe77ea4ca6863491276e4a2892c8d66
Maksim Zholudev <maximzms@gmail.com>
2025-12-03 02:17:02 +0300

    Experimental logger

--- logging/logging.hpp
+++ logging/logging.hpp
@@ -3,6 +3,8 @@
 #pragma once
 
 #include <string>
+#include <format>
+#include <mutex>
 
 namespace Istok::Logging {
 
@@ -16,11 +18,107 @@ enum class Level {
     TRACE = 6,
 };
 
-class Logger {};
+class Logger {
+public:
+    virtual ~Logger() = default;
 
-void set(std::string name, Logger& logger, Level maxLevel) {}
+    virtual void log(Level level, std::string_view message) = 0;
+};
+
+class NoneLogger final : public Logger {
+    void log(Level level, std::string_view message) override {}
+};
+
+NoneLogger none;
+
+class LoggerRegistry {
+public:
+    struct Entry {
+        Logger* logger;
+        Level maxLevel;
+    };
+
+    static LoggerRegistry& getGlobal() {
+        static LoggerRegistry instance;
+        return instance;
+    }
+
+    LoggerRegistry() {
+        set("", none, Level::OFF);
+    }
+
+    void set(std::string_view name, Logger& logger, Level maxLevel) {
+        std::lock_guard lock(mutex_);
+        loggers_[std::string(name)] = Entry{&logger, maxLevel};
+    }
+
+    Entry get(std::string_view name) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        do {
+            if (
+                auto it = loggers_.find(std::string(name));
+                it != loggers_.end()
+            ) {
+                return it->second;
+            }
+            
+            size_t pos = name.find_last_of('.');
+            if (pos == std::string_view::npos) {
+                pos = 0;
+            }
+            name = name.substr(0, pos);
+        } while (!name.empty());
+
+        assert(false);
+    }
+
+private:
+    std::unordered_map<std::string, Entry> loggers_;
+    std::mutex mutex_;
+};
 
 }  // namespace Istok::Logging
 
-#define WITH_LOGGER(name)  // TODO
-#define LOG_ERROR(...)  // TODO
+#define SET_LOGGER(name, logger_ref, maxLevel) \
+    Istok::Logging::LoggerRegistry::getGlobal().set(name, logger_ref, maxLevel)
+
+#define WITH_LOGGER(name) \
+    static Istok::Logging::LoggerRegistry::Entry istok_logging_get_logger = \
+        []() { \
+            static auto entry = \
+                Istok::Logging::LoggerRegistry::getGlobal().get(name); \
+            return entry; \
+        }
+
+#define WITH_THIS_LOGGER(name) \
+    static Istok::Logging::LoggerRegistry::Entry istok_logging_get_logger() { \
+        static auto entry = \
+            Istok::Logging::LoggerRegistry::getGlobal().get(name); \
+        return entry; \
+    }
+
+#define LOG_WITH_LEVEL(level, format, ...) \
+    do { \
+        if (level <= istok_logging_get_logger().maxLevel) { \
+            istok_logging_get_logger().logger->log( \
+                std::format(fmt, ##__VA_ARGS__)); \
+        } \
+    } while (0)
+
+#define LOG_CRITICAL(format, ...) \
+    LOG_WITH_LEVEL(Istok::Logging::Level::CRITICAL, format, ##__VA_ARGS__)
+
+#define LOG_ERROR(format, ...) \
+    LOG_WITH_LEVEL(Istok::Logging::Level::ERROR, format, ##__VA_ARGS__)
+
+#define LOG_WARNING(format, ...) \
+    LOG_WITH_LEVEL(Istok::Logging::Level::WARNING, format, ##__VA_ARGS__)
+
+#define LOG_INFO(format, ...) \
+    LOG_WITH_LEVEL(Istok::Logging::Level::INFO, format, ##__VA_ARGS__)
+
+#define LOG_DEBUG(format, ...) \
+    LOG_WITH_LEVEL(Istok::Logging::Level::DEBUG, format, ##__VA_ARGS__)
+
+#define LOG_TRACE(format, ...) \
+    LOG_WITH_LEVEL(Istok::Logging::Level::TRACE, format, ##__VA_ARGS__)
--- test/unit/logging/test_logging.cpp
+++ test/unit/logging/test_logging.cpp
@@ -12,9 +12,41 @@ namespace {
     struct MockLogger : public Logger {};
 }
 
-TEST_CASE("Logging", "[unit][logging]") {
-    MockLogger logger;
-    set("unittest", logger, Level::INFO);
-    WITH_LOGGER("unittest");
-    LOG_ERROR("foo");
+TEST_CASE("Logging - WITH_LOGGER", "[unit][logging]") {
+    MockLogger logger1;
+    MockLogger logger2;
+    MockLogger logger3;
+    SET_LOGGER("_test1", logger1, Level::OFF);
+    SET_LOGGER("_test2", logger2, Level::WARNING);
+    SET_LOGGER("_test3", logger3, Level::TRACE);
+    [](){
+        WITH_LOGGER("_test1");
+        int i = 1;
+        LOG_CRITICAL("critical {}", i);
+        LOG_ERROR("error {}", i);
+        LOG_WARNING("warning {}", i);
+        LOG_INFO("info {}", i);
+        LOG_DEBUG("debug {}", i);
+        LOG_TRACE("trace {}", i);
+    }();
+    [](){
+        WITH_LOGGER("_test2");
+        int i = 2;
+        LOG_CRITICAL("critical {}", i);
+        LOG_ERROR("error {}", i);
+        LOG_WARNING("warning {}", i);
+        LOG_INFO("info {}", i);
+        LOG_DEBUG("debug {}", i);
+        LOG_TRACE("trace {}", i);
+    }();
+    [](){
+        WITH_LOGGER("_test3");
+        int i = 3;
+        LOG_CRITICAL("critical {}", i);
+        LOG_ERROR("error {}", i);
+        LOG_WARNING("warning {}", i);
+        LOG_INFO("info {}", i);
+        LOG_DEBUG("debug {}", i);
+        LOG_TRACE("trace {}", i);
+    }();
 }
