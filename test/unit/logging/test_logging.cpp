aac59826d59d25977d4a784bf6cf16de65ac1aad
Maksim Zholudev <maximzms@gmail.com>
2025-12-04 01:16:55 +0300

    Add class logger test

--- test/unit/logging/test_logging.cpp
+++ test/unit/logging/test_logging.cpp
@@ -23,11 +23,11 @@ TEST_CASE("Logging - WITH_LOGGER", "[unit][logging]") {
     MockLogger logger1;
     MockLogger logger2;
     MockLogger logger3;
-    SET_LOGGER("_test1", logger1, Level::OFF);
-    SET_LOGGER("_test2", logger2, Level::WARNING);
-    SET_LOGGER("_test3", logger3, Level::TRACE);
+    SET_LOGGER("_test_logging_WITH_LOGGER_1", logger1, Level::OFF);
+    SET_LOGGER("_test_logging_WITH_LOGGER_2", logger2, Level::WARNING);
+    SET_LOGGER("_test_logging_WITH_LOGGER_3", logger3, Level::TRACE);
     [](){
-        WITH_LOGGER("_test1");
+        WITH_LOGGER("_test_logging_WITH_LOGGER_1");
         int i = 1;
         LOG_CRITICAL("critical {}", i);
         LOG_ERROR("error {}", i);
@@ -37,7 +37,7 @@ TEST_CASE("Logging - WITH_LOGGER", "[unit][logging]") {
         LOG_TRACE("trace {}", i);
     }();
     [](){
-        WITH_LOGGER("_test2");
+        WITH_LOGGER("_test_logging_WITH_LOGGER_2");
         int i = 2;
         LOG_CRITICAL("critical {}", i);
         LOG_ERROR("error {}", i);
@@ -47,7 +47,7 @@ TEST_CASE("Logging - WITH_LOGGER", "[unit][logging]") {
         LOG_TRACE("trace {}", i);
     }();
     [](){
-        WITH_LOGGER("_test3");
+        WITH_LOGGER("_test_logging_WITH_LOGGER_3");
         int i = 3;
         LOG_CRITICAL("critical {}", i);
         LOG_ERROR("error {}", i);
@@ -63,6 +63,124 @@ TEST_CASE("Logging - WITH_LOGGER", "[unit][logging]") {
         "3: warning 2",
     });
     REQUIRE(logger3.entries == std::vector<std::string>{
+        "1: critical 3",
+        "2: error 3",
+        "3: warning 3",
+        "4: info 3",
+        "5: debug 3",
+        "6: trace 3",
+    });
+}
+
+TEST_CASE("Logging - CLASS_WITH_LOGGER", "[unit][logging]") {
+    MockLogger logger1;
+    MockLogger logger2;
+    MockLogger logger3;
+    SET_LOGGER("_test_logging_CLASS_WITH_LOGGER_1", logger1, Level::OFF);
+    SET_LOGGER("_test_logging_CLASS_WITH_LOGGER_2", logger2, Level::WARNING);
+    SET_LOGGER("_test_logging_CLASS_WITH_LOGGER_3", logger3, Level::TRACE);
+    
+    class C1 {
+    public:
+        static void foo() {
+            int i = 1;
+            LOG_CRITICAL("static critical {}", i);
+            LOG_ERROR("static error {}", i);
+            LOG_WARNING("static warning {}", i);
+            LOG_INFO("static info {}", i);
+            LOG_DEBUG("static debug {}", i);
+            LOG_TRACE("static trace {}", i);
+        }
+        
+        void boo() {
+            int i = 1;
+            LOG_CRITICAL("critical {}", i);
+            LOG_ERROR("error {}", i);
+            LOG_WARNING("warning {}", i);
+            LOG_INFO("info {}", i);
+            LOG_DEBUG("debug {}", i);
+            LOG_TRACE("trace {}", i);
+        }
+    
+    private:
+        CLASS_WITH_LOGGER("_test_logging_CLASS_WITH_LOGGER_1");
+    };
+
+    class C2 {
+    public:
+        static void foo() {
+            int i = 2;
+            LOG_CRITICAL("static critical {}", i);
+            LOG_ERROR("static error {}", i);
+            LOG_WARNING("static warning {}", i);
+            LOG_INFO("static info {}", i);
+            LOG_DEBUG("static debug {}", i);
+            LOG_TRACE("static trace {}", i);
+        }
+        
+        void boo() {
+            int i = 2;
+            LOG_CRITICAL("critical {}", i);
+            LOG_ERROR("error {}", i);
+            LOG_WARNING("warning {}", i);
+            LOG_INFO("info {}", i);
+            LOG_DEBUG("debug {}", i);
+            LOG_TRACE("trace {}", i);
+        }
+    
+    private:
+        CLASS_WITH_LOGGER("_test_logging_CLASS_WITH_LOGGER_2");
+    };
+
+    class C3 {
+    public:
+        static void foo() {
+            int i = 3;
+            LOG_CRITICAL("static critical {}", i);
+            LOG_ERROR("static error {}", i);
+            LOG_WARNING("static warning {}", i);
+            LOG_INFO("static info {}", i);
+            LOG_DEBUG("static debug {}", i);
+            LOG_TRACE("static trace {}", i);
+        }
+        
+        void boo() {
+            int i = 3;
+            LOG_CRITICAL("critical {}", i);
+            LOG_ERROR("error {}", i);
+            LOG_WARNING("warning {}", i);
+            LOG_INFO("info {}", i);
+            LOG_DEBUG("debug {}", i);
+            LOG_TRACE("trace {}", i);
+        }
+    
+    private:
+        CLASS_WITH_LOGGER("_test_logging_CLASS_WITH_LOGGER_3");
+    };
+
+    C1::foo();
+    C1().boo();
+    C2::foo();
+    C2().boo();
+    C3::foo();
+    C3().boo();
+
+    REQUIRE(logger1.entries == std::vector<std::string>{});
+    REQUIRE(logger2.entries == std::vector<std::string>{
+        "1: static critical 2",
+        "2: static error 2",
+        "3: static warning 2",
+        "1: critical 2",
+        "2: error 2",
+        "3: warning 2",
+    });
+    REQUIRE(logger3.entries == std::vector<std::string>{
+        "1: static critical 3",
+        "2: static error 3",
+        "3: static warning 3",
+        "4: static info 3",
+        "5: static debug 3",
+        "6: static trace 3",
         "1: critical 3",
         "2: error 3",
         "3: warning 3",
