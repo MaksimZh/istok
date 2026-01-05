176a19fd921487a6344b69d7ee867fe4acda4742
Maksim Zholudev <maximzms@gmail.com>
2026-01-05 22:36:50 +0300

    Add new EntityManager

--- ecs2/include/internal/entity.hpp
+++ ecs2/include/internal/entity.hpp
@@ -1,8 +1,10 @@
 // Copyright 2026 Maksim Sergeevich Zholudev. All rights reserved
 #pragma once
 
+#include <bit>
+#include <cassert>
 #include <cstdint>
-#include <functional>
+#include <vector>
 
 
 namespace Istok::ECS {
@@ -10,27 +12,99 @@ namespace Istok::ECS {
 struct Entity {
     struct Hasher {
         size_t operator()(const Entity& entity) const {
-            return std::hash<uint64_t>()(entity.value_);
+            return std::bit_cast<size_t>(entity);
         }
     };
 
-    constexpr Entity() : value_(0) {}
     constexpr Entity(size_t index, size_t generation)
-        : value_(index + (generation << 32)) {}
+    : index_(index), generation_(generation) {}
+    
     bool operator==(const Entity& other) const = default;
 
     uint32_t index() const {
-        return value_ & LOWER_MASK;
+        return index_;
     }
 
     uint32_t generation() const {
-        return (value_ & UPPER_MASK) >> 32;
+        return generation_;
     }
 
 private:
-    static constexpr uint64_t LOWER_MASK = 0x00000000ffffffff;
-    static constexpr uint64_t UPPER_MASK = 0xffffffff00000000;
-    uint64_t value_;
+    int32_t index_;
+    int32_t generation_;
+};
+
+
+class EntityManager {
+public:
+    bool isValidEntity(Entity entity) const {
+        return entity.index() < cells_.size()
+            && std::bit_cast<Entity>(cells_[entity.index()]) == entity;
+    }
+    
+    Entity createEntity() {
+        if (freeIndex_ == cells_.size()) {
+            cells_.push_back(Cell{freeIndex_, 0});
+            return getEntity(freeIndex_++);
+        }
+        auto index = freeIndex_;
+        freeIndex_ = getLink(freeIndex_);
+        reviveEntity(index);
+        return getEntity(index);
+    }
+
+    void deleteEntity(Entity entity) {
+        if (!isValidEntity(entity)) {
+            return;
+        }
+        setLink(entity.index(), freeIndex_);
+        freeIndex_ = entity.index();
+    }
+
+private:
+    struct Cell {
+        int32_t index;
+        int32_t generation;
+    };
+
+    std::vector<Cell> cells_;
+    int32_t freeIndex_ = 0;
+
+    bool isLink(int32_t index) const {
+        assert(index >= 0);
+        assert(index < cells_.size());
+        return cells_[index].index < 0;
+    }
+
+    Entity getEntity(int32_t index) const {
+        assert(index >= 0);
+        assert(index < cells_.size());
+        assert(!isLink(index));
+        return Entity(cells_[index].index, cells_[index].generation);
+    }
+
+    int32_t getLink(int32_t index) const {
+        assert(index >= 0);
+        assert(index < cells_.size());
+        assert(isLink(index));
+        return -cells_[index].index - 1;
+    }
+
+    void setLink(int32_t index, int32_t target) {
+        assert(index >= 0);
+        assert(index < cells_.size());
+        assert(target >= 0);
+        assert(target <= cells_.size());
+        cells_[index].index = -target - 1;
+    }
+
+    void reviveEntity(int32_t index) {
+        assert(index >= 0);
+        assert(index < cells_.size());
+        assert(isLink(index));
+        cells_[index].index = index;
+        ++cells_[index].generation;
+    }
 };
 
 }  // namespace Istok::ECS
--- ecs2/src/entity_unittest.cpp
+++ ecs2/src/entity_unittest.cpp
@@ -2,19 +2,106 @@
 #include <catch.hpp>
 
 #include <catch2/catch_test_macros.hpp>
-#include <ecs.hpp>
+#include <cstdint>
+#include <internal/entity.hpp>
+
+#include <set>
 
 using namespace Istok::ECS;
 
 
-TEST_CASE("Entity - create", "[unit][ecs]") {
-    Entity e(42, 17);
-    REQUIRE(e.index() == 42);
-    REQUIRE(e.generation() == 17);
+TEST_CASE("Entity", "[unit][ecs]") {
+    SECTION("getters") {
+        Entity e(42, 17);
+        REQUIRE(e.index() == 42);
+        REQUIRE(e.generation() == 17);
+    }
+    
+    SECTION("equality", "[unit][ecs]") {
+        REQUIRE(Entity(42, 17) == Entity(42, 17));
+        REQUIRE(Entity(42, 17) != Entity(24, 17));
+        REQUIRE(Entity(42, 17) != Entity(42, 71));
+    }
 }
 
-TEST_CASE("Entity - compare", "[unit][ecs]") {
-    REQUIRE(Entity(42, 17) == Entity(42, 17));
-    REQUIRE(Entity(42, 17) != Entity(24, 17));
-    REQUIRE(Entity(42, 17) != Entity(42, 71));
+
+TEST_CASE("EntityManager - basics", "[unit][ecs]") {
+    EntityManager em;
+    Entity a = em.createEntity();
+    Entity b = em.createEntity();
+    Entity c = em.createEntity();
+    
+    SECTION("validity") {
+        REQUIRE(em.isValidEntity(a));
+        REQUIRE(em.isValidEntity(b));
+        REQUIRE(em.isValidEntity(c));
+    }
+
+    SECTION("uniqueness") {
+        REQUIRE(a != b);
+        REQUIRE(b != c);
+        REQUIRE(c != a);
+    }
+
+    SECTION("invalidate on deletion") {
+        em.deleteEntity(b);
+        REQUIRE(em.isValidEntity(a));
+        REQUIRE(!em.isValidEntity(b));
+        REQUIRE(em.isValidEntity(c));
+    }
+
+    SECTION("deletion is idempotent") {
+        em.deleteEntity(b);
+        em.deleteEntity(b);
+        REQUIRE(em.isValidEntity(a));
+        REQUIRE(!em.isValidEntity(b));
+        REQUIRE(em.isValidEntity(c));
+    }
+
+    SECTION("reuse after deletion") {
+        em.deleteEntity(b);
+        Entity d = em.createEntity();
+        REQUIRE(em.isValidEntity(a));
+        REQUIRE(!em.isValidEntity(b));
+        REQUIRE(em.isValidEntity(c));
+        REQUIRE(em.isValidEntity(d));
+        REQUIRE(d != a);
+        REQUIRE(d != b);
+        REQUIRE(d != c);
+        REQUIRE(d.index() == b.index());
+    }
+}
+
+
+TEST_CASE("EntityManager - mass index reuse", "[unit][ecs]") {
+    EntityManager em;
+    Entity a = em.createEntity();
+    Entity b = em.createEntity();
+    Entity c = em.createEntity();
+    em.deleteEntity(a);
+    em.deleteEntity(b);
+    em.deleteEntity(c);
+    Entity d = em.createEntity();
+    Entity e = em.createEntity();
+    Entity f = em.createEntity();
+    em.deleteEntity(d);
+    em.deleteEntity(e);
+    em.deleteEntity(f);
+    Entity g = em.createEntity();
+    Entity h = em.createEntity();
+    Entity i = em.createEntity();
+    REQUIRE(!em.isValidEntity(a));
+    REQUIRE(!em.isValidEntity(b));
+    REQUIRE(!em.isValidEntity(c));
+    REQUIRE(!em.isValidEntity(d));
+    REQUIRE(!em.isValidEntity(e));
+    REQUIRE(!em.isValidEntity(f));
+    REQUIRE(em.isValidEntity(g));
+    REQUIRE(em.isValidEntity(h));
+    REQUIRE(em.isValidEntity(i));
+    std::set<uint32_t> x = {a.index(), b.index(), c.index()};
+    std::set<uint32_t> y = {d.index(), e.index(), f.index()};
+    std::set<uint32_t> z = {g.index(), h.index(), i.index()};
+    REQUIRE(x == y);
+    REQUIRE(y == z);
 }
