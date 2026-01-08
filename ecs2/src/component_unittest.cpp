226258ea752406f11ae24a8feb90869220b40f2d
Maksim Zholudev <maximzms@gmail.com>
2026-01-08 23:47:01 +0300

    Improve component storage unittests

--- ecs2/src/component_unittest.cpp
+++ ecs2/src/component_unittest.cpp
@@ -63,4 +63,81 @@ TEST_CASE("ComponentStorageOf - basics", "[unit][ecs]") {
         REQUIRE(*cs.get(2) == 42);
         REQUIRE(*cs.get(3) == 43);
     }
+
+    SECTION("remove twice") {
+        cs.insert(0, 40);
+        cs.insert(1, 41);
+        cs.insert(2, 42);
+        cs.remove(1);
+        cs.remove(1);
+        REQUIRE(cs.has(0));
+        REQUIRE(!cs.has(1));
+        REQUIRE(cs.has(2));
+        REQUIRE(*cs.get(0) == 40);
+        REQUIRE(*cs.get(2) == 42);
+    }
+
+    SECTION("remove empty") {
+        cs.insert(0, 40);
+        cs.insert(1, 41);
+        cs.insert(2, 42);
+        cs.remove(3);
+        REQUIRE(cs.has(0));
+        REQUIRE(cs.has(1));
+        REQUIRE(cs.has(2));
+        REQUIRE(*cs.get(0) == 40);
+        REQUIRE(*cs.get(1) == 41);
+        REQUIRE(*cs.get(2) == 42);
+    }
+}
+
+
+TEST_CASE("ComponentStorageOf - multi actions", "[unit][ecs]") {
+    ComponentStorageOf<int> cs;
+    cs.insert(0, 100);
+    cs.insert(3, 103);
+    cs.insert(2, 102);
+    cs.insert(9, 109);
+    cs.insert(6, 106);
+    cs.insert(7, 107);
+    REQUIRE(cs.has(0));
+    REQUIRE(!cs.has(1));
+    REQUIRE(cs.has(2));
+    REQUIRE(cs.has(3));
+    REQUIRE(!cs.has(4));
+    REQUIRE(!cs.has(5));
+    REQUIRE(cs.has(6));
+    REQUIRE(cs.has(7));
+    REQUIRE(!cs.has(8));
+    REQUIRE(cs.has(9));
+    REQUIRE(*cs.get(0) == 100);
+    REQUIRE(*cs.get(2) == 102);
+    REQUIRE(*cs.get(3) == 103);
+    REQUIRE(*cs.get(6) == 106);
+    REQUIRE(*cs.get(7) == 107);
+    REQUIRE(*cs.get(9) == 109);
+    cs.remove(2);
+    cs.remove(9);
+    cs.remove(4);
+    cs.remove(6);
+    cs.insert(0, 200);
+    cs.insert(5, 205);
+    cs.insert(2, 202);
+    cs.insert(8, 208);
+    REQUIRE(cs.has(0));
+    REQUIRE(!cs.has(1));
+    REQUIRE(cs.has(2));
+    REQUIRE(cs.has(3));
+    REQUIRE(!cs.has(4));
+    REQUIRE(cs.has(5));
+    REQUIRE(!cs.has(6));
+    REQUIRE(cs.has(7));
+    REQUIRE(cs.has(8));
+    REQUIRE(!cs.has(9));
+    REQUIRE(*cs.get(0) == 200);
+    REQUIRE(*cs.get(2) == 202);
+    REQUIRE(*cs.get(3) == 103);
+    REQUIRE(*cs.get(5) == 205);
+    REQUIRE(*cs.get(7) == 107);
+    REQUIRE(*cs.get(8) == 208);
 }
