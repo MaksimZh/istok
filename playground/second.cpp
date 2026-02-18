0800679971509c91757eedc94c248bf04e90a131
Maksim Zholudev <maximzms@gmail.com>
2026-02-18 13:13:45 +0300

    Add dispatcher MVP

--- playground/second.cpp
+++ playground/second.cpp
@@ -25,6 +25,29 @@ private:
     WindowMessageHandlerFunc func_;
 };
 
+using EntityWindowMessageHandlerFunc =
+    std::function<LRESULT(ECS::Entity, WinAPI::WindowMessage)>;
+
+class EntityWindowMessageDispatcher {
+public:
+    LRESULT handleMessage(
+        ECS::Entity entity, WinAPI::WindowMessage message
+    ) noexcept {
+        auto it = handlers_.find(message.msg);
+        if (it == handlers_.end()) {
+            return WinAPI::handleMessageByDefault(message);
+        }
+        return it->second(entity, message);
+    }
+
+    void setHandler(UINT msg, EntityWindowMessageHandlerFunc func) {
+        handlers_[msg] = func;
+    }
+
+private:
+    std::unordered_map<UINT, EntityWindowMessageHandlerFunc> handlers_;
+};
+
 int main() {
     SET_LOGGER(
         "",
@@ -33,19 +56,27 @@ int main() {
     WITH_LOGGER_PREFIX("", "App: ");
     LOG_TRACE("begin");
 
+    ECS::ECSManager ecs;
+    
+    EntityWindowMessageDispatcher dispatcher;
+    dispatcher.setHandler(
+        WM_DESTROY,
+        [](ECS::Entity entity, WinAPI::WindowMessage message) -> LRESULT {
+            assert(message.msg == WM_DESTROY);
+            PostQuitMessage(0);
+            return 0;
+        });
+
+    auto dummyEntity = ecs.createEntity();
+
     WinAPI::WndHandle wnd({100, 100, 400, 300});
     ShowWindow(wnd.getHWnd(), SW_SHOW);
     WindowMessageHandlerProxy handler(
-        [](WinAPI::WindowMessage message) -> LRESULT {
-            if (message.msg == WM_DESTROY) {
-                PostQuitMessage(0);
-                return 0;
-            }
-            return WinAPI::handleMessageByDefault(message);
+        [&](WinAPI::WindowMessage message) -> LRESULT {
+            return dispatcher.handleMessage(dummyEntity, message);
         });
     wnd.setHandler(&handler);
 
-    ECS::ECSManager ecs;
     ecs.addLoopSystem([](ECS::ECSManager& ecs) {
         LOG_TRACE("loop");
     });
@@ -58,6 +89,7 @@ int main() {
         MSG msg;
         GetMessage(&msg, NULL, 0, 0);
         if (msg.message == WM_QUIT) {
+            LOG_TRACE("WM_QUIT message received");
             break;
         }
         TranslateMessage(&msg);
